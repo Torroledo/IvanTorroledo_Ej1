1e59dfc78389fd47d63ca37928ffdc055f6498b6
Iván Torroledo Peña <ivan.torroledo@gmail.com>
2017-02-12 18:45:13 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('initial_data.txt')
+data = np.genfromtxt('data.txt')
 size = int(np.sqrt(data.shape[0]))
 potential = data.reshape(size, size)
 
